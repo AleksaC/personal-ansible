2c19aefc4709b9f8b10de84a8a00992f886a757c
AleksaC <aleksacukovic1@gmail.com>
2020-08-09 23:27:29 +0200

    Minor fixes

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ set -eou pipefail
 
 # Temporarily disable unattended upgrades so they don't acquire dpkg lock between ansible tasks
 old_config=$(< /etc/apt/apt.conf.d/20auto-upgrades)
-sudo sed -i 's/1/0/g' /etc/apt/apt.conf.d/20auto-upgrades
 
 restore_config() {
   echo "$old_config" | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
@@ -12,6 +11,8 @@ restore_config() {
 
 trap restore_config EXIT
 trap restore_config ERR
+
+sudo sed -i 's/1/0/g' /etc/apt/apt.conf.d/20auto-upgrades
 
 sudo apt update
 
@@ -34,15 +35,13 @@ if ! command -v ansible-playbook > /dev/null; then
   pip install wheel
   pip install psutil ansible
 
-  # python-apt is required to use apt module; seems like it cannot be installed through pip
-  # this probably only works on ubuntu 20
   mkdir py-apt
   sudo chown _apt py-apt
 
   cd py-apt
 
   sudo apt download python3-apt=2.0.0
-  dpkg -x python3-apt_2.0.0_amd64.deb python-apt
+  dpkg -x python3-apt_*.deb python-apt
   cp -r ./python-apt/usr/lib/python3/dist-packages/* ../venv/lib/python3.8/site-packages/
 
   cd ..
