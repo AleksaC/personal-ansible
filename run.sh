71bffc8013e90021775afea4c828bcb8c912cf89
AleksaC <aleksacukovic1@gmail.com>
2021-01-08 23:03:55 +0100

    Move venv setup to separate script and add option to specify clone destination

--- run.sh
+++ run.sh
@@ -14,39 +14,27 @@ trap restore_config ERR
 
 sudo sed -i 's/1/0/g' /etc/apt/apt.conf.d/20auto-upgrades
 
-sudo apt update
+sudo apt-get update
 
 if [ ! -t 0 ]; then
+  dest=${1:-/tmp/personal-ansible}
+  [[ "$dest" == */  ]] && dest="${dest::-1}"
+
   if ! command -v git > /dev/null; then
-    sudo apt install -y git
+    sudo apt-get install -y git
   fi
 
-  git clone https://github.com/AleksaC/personal-ansible /tmp/personal-ansible
+  git clone https://github.com/AleksaC/personal-ansible "$dest"
 
-  cd /tmp/personal-ansible
+  cd "$dest"
 fi
 
 if ! command -v ansible-playbook > /dev/null; then
-  sudo apt install -y build-essential python3-virtualenv python3-dev
-
-  virtualenv venv -p python3
-  source ./venv/bin/activate
-
-  pip install wheel
-  pip install psutil "ansible>=2.9<2.11"
-
-  mkdir py-apt
-  sudo chown _apt py-apt
-
-  cd py-apt
-
-  sudo apt download python3-apt
-  dpkg -x python3-apt_*.deb python-apt
-  cp -r ./python-apt/usr/lib/python3/dist-packages/* ../venv/lib/python3.8/site-packages/
-
-  rm -rf ./python3-apt_*.dev ./python-apt
-
-  cd ..
+  if [ -x venv/bin/ansible-playbook ]; then
+    source venv/bin/activate
+  else
+    source setup-venv.sh
+  fi
 fi
 
 ansible-playbook main.yml -K
---
+++ setup-venv.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+vnv=virtualenv
+
+if ! command -v virtualenv > /dev/null; then
+  sudo apt-get install python3-distutils
+  wget -O /tmp/virtualenv.pyz https://bootstrap.pypa.io/virtualenv.pyz
+  vnv="python /tmp/virtualenv.pyz"
+fi
+
+$vnv venv -p python3
+source ./venv/bin/activate
+
+mkdir py-apt
+sudo chown _apt py-apt
+
+cd py-apt
+
+sudo apt-get download python3-apt
+dpkg -x python3-apt_*.deb python-apt
+cp -r ./python-apt/usr/lib/python3/dist-packages/* ../venv/lib/python3.8/site-packages/
+
+cd ..
+
+rm -rf py-apt
+
+pip install wheel psutil "ansible>=2.9<2.11"
