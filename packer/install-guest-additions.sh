00f9f125bf053f1247ad79a1367b6b637a4eca11
AleksaC <aleksacukovic1@gmail.com>
2021-01-23 00:59:16 +0100

    Add packer setup for virtualbox Vagrant box

---
+++ packer/install-guest-additions.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+set -eou pipefail
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get install build-essential dkms linux-headers-$(uname -r) -y
+
+sudo mkdir /media/vboxga
+sudo mount -o loop $HOME/VBoxGuestAdditions.iso /media/vboxga
+
+sudo /media/vboxga/VBoxLinuxAdditions.run || true
+
+if ! modinfo vboxsf &> /dev/null; then
+  echo "Cannot find vbox kernel module. Installation of guest additions unsuccessful!"
+  exit 1
+fi
+
+sudo adduser $USER vboxsf
