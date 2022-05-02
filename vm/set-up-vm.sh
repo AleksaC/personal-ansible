6574eb2838dcec23fb40423d36ce504d650231e4
AleksaC <aleksacukovic1@gmail.com>
2022-05-02 18:46:19 +0200

    Update packer & vagrant config to ubuntu 22.04

--- vm/set-up-vm.sh
+++ vm/set-up-vm.sh
@@ -4,12 +4,12 @@ set -eou pipefail
 
 cd "$(dirname "$0")"
 
-if ! vagrant box list | grep ubuntu-20.04 &> /dev/null; then
-  if [ ! -f ubuntu-20-04.box ]; then
-    packer build ubuntu-20-04.json
+if ! vagrant box list | grep ubuntu-22.04 &> /dev/null; then
+  if [ ! -f ubuntu-22-04.box ]; then
+    packer build ubuntu-22-04.json
   fi
 
-  vagrant box add --name ubuntu-20.04 "file://$(readlink -f ubuntu-20-04.box)"
+  vagrant box add --name ubuntu-22.04 "file://$(readlink -f ubuntu-22-04.box)"
 fi
 
 vagrant up
